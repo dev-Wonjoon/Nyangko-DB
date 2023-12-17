c2c8a68b79db9455d7baee2837a35a793e3c43e9
dev-Wonjoon <dnjswns775@gmail.com>
2023-12-18 03:20:01 +0900

    init commit

---
+++ src/main/java/devjoon/nyangkodb/NyangkodbApplication.java
@@ -0,0 +1,13 @@
+package devjoon.nyangkodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangkodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyangkodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devjoon/nyangkodb/NyangkodbApplicationTests.java
@@ -0,0 +1,13 @@
+package devjoon.nyangkodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangkodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
